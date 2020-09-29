84fb99dc59db17f7e0e2648af7a06a3fa2a33ba0
Jonathan Sarco <jfe.sarco@gmail.com>
2020-09-29 19:42:30 -0300

    Fix in Toggle

--- src/Components/Navigation/Toggle/Toggle.jsx
+++ src/Components/Navigation/Toggle/Toggle.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import LanguageToggle from '../../UI/LanguageToggle/index';
 import { ToggleContainer } from './css';
 import { useTranslation } from 'react-i18next';
 
 function Toggle() {
     const { i18n } = useTranslation();
-
+    
     const setLanguage = (id) => {
         i18n.changeLanguage(id);
     }
 
     return (
         <ToggleContainer>
-            <LanguageToggle language={i18n.language.split(0, 1)[0]} setlanguage={setLanguage} />
+            <LanguageToggle language={i18n.language.split('-')[0]} setlanguage={setLanguage} />
         </ToggleContainer>
     );
 }
